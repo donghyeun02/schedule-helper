ffbe8a7e16d893083413635a747b47b2960085f6
신동현 <donghyeun02@gmail.com>
2023-09-14 13:04:32 +0900

    Modify : home page 엔드포인트 삭제 및 로그인 로직 url 수정

--- routes/calendarRouter.js
+++ routes/calendarRouter.js
@@ -3,7 +3,6 @@ const calendarService = require('../services/calendarService');
 
 const router = express.Router();
 
-router.get('/', calendarService.homePage);
 router.get('/login', calendarService.googleLogin);
 router.get('/authcode', calendarService.getToken);
 

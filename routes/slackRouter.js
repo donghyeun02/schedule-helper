f16115b420bf4f2a0bdca2f022773739308a7428
신동현 <donghyeun02@gmail.com>
2023-09-21 16:59:38 +0900

    Add : 이벤트 발생 시 이벤트 종류에 따른 슬랙 메시지 보내기

--- routes/slackRouter.js
+++ routes/slackRouter.js
@@ -4,6 +4,5 @@ const slackService = require('../services/slackService');
 const router = express.Router();
 
 router.use('/events', slackService.eventSubscriptions);
-router.post('/message', slackService.sendMessage);
 
 module.exports = router;
--- utils/oauth2.js
+++ utils/oauth2.js
@@ -1,1 +1,13 @@
-// oauth2.js
+const { google } = require('googleapis');
+
+const clientId = process.env.GOOGLE_CLIENT_ID;
+const clientSecret = process.env.GOOGLE_SECRET;
+const oauthRedirectUri = process.env.OAUTH_REDIRECT_URI;
+
+const oauth2Client = new google.auth.OAuth2(
+  clientId,
+  clientSecret,
+  oauthRedirectUri
+);
+
+module.exports = { oauth2Client };
